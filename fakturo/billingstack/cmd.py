7f4f13a4de6df924da3dd6fa2cb558fb06c39736
Endre Karlson <endre.karlson@gmail.com>
2013-01-17 20:22:58 +0100

    Merchant and Customer commands

--- fakturo/billingstack/cmd.py
+++ fakturo/billingstack/cmd.py
@@ -2,9 +2,11 @@ class CommandApi(object):
     def __init__(self, client):
         self.client = client
 
-    def merchant_list(self, parsed_args, command):
-        return self.client.merchant.list()
+    @classmethod
+    def _add_id(cls, parser):
+        parser.add_argument('id')
 
+    # Merchant commands
     @staticmethod
     def merchant_create_parser(parser):
         parser.add_argument('--username')
@@ -20,3 +22,89 @@ class CommandApi(object):
             currency=parsed_args.currency
         )
         return self.client.merchant.create(values)
+
+    def merchant_list(self, parsed_args, command):
+        return self.client.merchant.list()
+
+    @staticmethod
+    def merchant_get_parser(parser):
+        parser.add_argument('id')
+
+    def merchant_get(self, parsed_args, command):
+        return self.client.merchant.get(parsed_args.id)
+
+    @staticmethod
+    def merchant_update_parser(parser):
+        parser.add_argument('id')
+        parser.add_argument('--language')
+        parser.add_argument('--currency')
+
+    def merchant_update(self, parsed_args, command):
+        """
+        Update a Merchant
+        """
+        values = dict(
+            email=parsed_args.email,
+            currency=parsed_args.currency)
+        return self.client.merchant.update(parsed_args.id, values)
+
+    @staticmethod
+    def merchant_delete_parser(parser):
+        parser.add_argument('id')
+
+    def merchant_delete(self, parsed_args, command):
+        return self.client.merchant.delete(parsed_args.id)
+
+    # Customer commands
+    @staticmethod
+    def customer_create_parser(parser):
+        parser.add_argument('--username')
+        parser.add_argument('--password')
+        parser.add_argument('--email')
+        parser.add_argument('--currency', default='NOK')
+
+    def customer_create(self, parsed_args, command):
+        values = dict(
+            username=parsed_args.username,
+            password=parsed_args.password,
+            email=parsed_args.email,
+            currency=parsed_args.currency)
+        return self.client.customer.create(values)
+
+    @staticmethod
+    def customer_list_parser(parser):
+        parser.add_argument('merchant_id')
+
+    def customer_list(self, parsed_args, command):
+        return self.client.customer.list(parsed_args.merchant_id)
+
+    @staticmethod
+    def customer_get_parser(parser):
+        parser.add_argument('merchant_id')
+
+    def customer_get(self, parsed_args, command):
+        return self.client.customer.get(parsed_args.merchant_id, parsed_args.id)
+
+    @staticmethod
+    def customer_update_parser(parser):
+        parser.add_argument('merchant_id')
+        parser.add_argument('id')
+
+    def customer_update(self, parsed_args, command):
+        values = dict(
+            password=parsed_args.password,
+            email=parsed_args.email,
+            currency=parsed_args.currency)
+
+        return self.client.customer.update(
+            parsed_args.merchant_id,
+            parsed_args.id,
+            values)
+
+    @staticmethod
+    def customer_delete_parser(parser):
+        parser.add_argument('merchant_id')
+        parser.add_argument('id')
+
+    def customer_delete(self, parsed_args, command):
+        return self.client.customer.delete(parsed_args.id, parsed_args.merchant_id)
--- fakturo/billingstack/controller.py
+++ fakturo/billingstack/controller.py
@@ -7,10 +7,43 @@ class Base(object):
 
 
 class Merchant(Base):
-    def create(self, merchant):
-        response = self.client.post('/merchants', data=json.dumps(merchant))
+    def create(self, values):
+        response = self.client.post('/merchants', data=json.dumps(values))
         return response.json
 
     def list(self):
         response = self.client.get('/merchants')
         return response.json
+
+    def get(self, id_):
+        response = self.client.get('/%s' % id_)
+        return response.json
+
+    def update(self, id_, values):
+        response = self.client.update('/%s' % id_, data=json.dumps(values))
+        return response.json
+
+    def delete(self, id_):
+        self.client.delete('/%s' % id_)
+
+
+class Customer(Base):
+    def create(self, merchant_id, values):
+        response = self.client.post('/s/customers' % merchant_id,
+                                    data=json.dumps(values))
+        return response.json
+
+    def list(self, merchant_id):
+        response = self.client.get('/%s/customers' % merchant_id)
+        return response.json
+
+    def get(self, merchant_id, id_):
+        response = self.client.get('/%s/customers/%s' % merchant_id, id_)
+        return response.json
+
+    def update(self, merchant_id, id_, values):
+        response = self.client.update('/%s/customers/%s' % (merchant_id, id_), data=json.dumps(values))
+        return response.json
+
+    def delete(self, merchant_id, id_):
+        self.client.delete('/%s/customers/%s' % (merchant_id, id_))

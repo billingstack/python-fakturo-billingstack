78c5b4bcdb7821e45399c17e93d786371b6f3e98
Endre Karlson <endre.karlson@gmail.com>
2013-01-18 16:01:24 +0100

    Update doc and remove merchant_id from create and list

--- fakturo/billingstack/controller.py
+++ fakturo/billingstack/controller.py
@@ -9,6 +9,7 @@ LOG = logging.getLogger(__name__)
 class Base(object):
     resource = None
     url = None
+
     parent = None
 
     def __init__(self, client):
@@ -18,6 +19,9 @@ class Base(object):
     def _item_url(cls):
         """
         Return the url part for a single resource of this class
+
+        :return: URL For a Item
+        :rtype: string
         """
         if cls.resource:
             id_key = '%(' + cls.resource[0:-1] + '_id)s'
@@ -29,6 +33,9 @@ class Base(object):
     def get_url(cls, item=False):
         """
         Get the URL for a collection / resource in this Controller
+
+        :param item: Retrieve of URL for a specific Item or Collection
+        :type item: bool
         """
         i = 0
         reversed = []
@@ -64,10 +71,16 @@ class Base(object):
 
     @property
     def collection_url(self):
+        """
+        Return the collection URL for this Controller
+        """
         return self.get_url()
 
     @classmethod
     def get_name(cls):
+        """
+        Get the Name of this Controller
+        """
         return re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r'_\1',
                       cls.__name__).lower()
 
@@ -83,7 +96,7 @@ class Base(object):
         return response
 
     def _list(self, url_data):
-        response = self._wrap_api_call(self.client.list, self.collection_url,
+        response = self._wrap_api_call(self.client.get, self.collection_url,
                                        url_data)
         return response
 
@@ -105,10 +118,10 @@ class Base(object):
 class Merchant(Base):
     url = '/%(merchant_id)s'
 
-    def create(self, merchant_id, values):
+    def create(self, values):
         return self._create(locals(), values).json
 
-    def list(self, merchant_id):
+    def list(self):
         return self._list(locals()).json
 
     def get(self, merchant_id):

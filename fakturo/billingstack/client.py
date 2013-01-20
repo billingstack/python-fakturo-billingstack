12fa7a0b09877b79be811ab1aaeba9edf01dbd05
Endre Karlson <endre.karlson@gmail.com>
2013-01-20 19:15:28 +0100

    Cleanup

--- fakturo/billingstack/client.py
+++ fakturo/billingstack/client.py
@@ -1,7 +1,6 @@
 import logging
 
 from fakturo.core import client
-from fakturo.core import exceptions
 from fakturo.billingstack import auth, controller
 
 
@@ -9,11 +8,11 @@ LOG = logging.getLogger(__name__)
 
 
 class Client(client.BaseClient):
-    def __init__(self, url=None, username=None, password=None, merchant=None):
+    def __init__(self, url=None, **kw):
         super(Client, self).__init__(url)
 
-        self.requests.auth = auth.AuthHelper(
-            url, username=username, password=password, merchant=merchant)
+        # NOTE: Helper object that's set on requests.auth
+        self.requests.auth = auth.AuthHelper(url, **kw)
 
         ctrls = controller.__all__
         LOG.debug('Loading Controllers: %s' % [c.get_name() for c in ctrls])
@@ -21,14 +20,10 @@ class Client(client.BaseClient):
         for cls in ctrls:
             setattr(self, cls.get_name(), cls(self))
 
-    def wrap_api_call(self, func, path, *args, **kw):
-        """
-        Wrap a self.<rest function> with exception handling
+    @property
+    def merchant_id(self):
+        return self.requests.auth.merchant.get('id')
 
-        :param func: The function to wrap
-        """
-        auth_merchant = self.requests.auth.auth_info.get('merchant', None)
-        if self.requests.auth.merchant_valid:
-            path = '/' + auth_merchant['id'] + path
-        response = super(Client, self).wrap_api_call(func, path, *args, **kw)
-        return response
+    @property
+    def customer_id(self):
+        return self.requests.auth.customer.get('id')
